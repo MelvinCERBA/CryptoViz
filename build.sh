68bcf509c4db846e2cbb5d76c30d3b8af3d42a85
MelvinCERBA <melvin.cerba@gmail.com>
2023-11-30 09:46:17 +0100

    build.sh script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# downs all containers if they're up
+# ./down.sh
+
+# Function to check if a Docker network exists
+check_network() {
+    docker network ls --filter name=$1 -q | wc -l
+}
+
+# Create "confluent-network" if it doesn't exist
+if [ $(check_network "confluent-network") -eq 0 ]; then
+    echo "Creating confluent-network..."
+    docker network create confluent-network
+fi
+
+# Create "db-network" if it doesn't exist
+if [ $(check_network "db-network") -eq 0 ]; then
+    echo "Creating db-network..."
+    docker network create db-network
+fi
+
+# Create "api-network" if it doesn't exist
+if [ $(check_network "api-network") -eq 0 ]; then
+    echo "Creating api-network..."
+    docker network create api-network
+fi
+
+# Remove all exited docker containers
+docker ps -a --filter "status=exited" -q | xargs -r docker rm
+
+# Array of specific directories with Docker Compose projects, in the order they should be started
+declare -a dirs=("_Infra/confluent" "_Infra/db" "API/" "Scrapper/" "SparksApp/" "WebApp/")
+
+# build Docker Compose projects in specified directories
+for dir in "${dirs[@]}"; do
+    if [ -f "$dir/docker-compose.yml" ]; then
+        echo " "
+        echo -e "\e[1mStarting Docker Compose project in \e[33m$dir\e[0m"
+        echo "- - - - - - - - - - - - - - - - - - - -"
+        (cd "$dir" && docker-compose build --no-cache)
+    else
+        echo "No docker-compose.yml found in $dir. Skipping..."
+    fi
+done
